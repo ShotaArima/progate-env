df6ebdaabcfac974c3af4301cf85cf62a011adf7
ShotaArima <g2172010@tcu.ac.jp>
2025-07-10 23:47:52 +0900

    format src/format/main.ts by Prettier

--- vscode_tutorial_get_starting_with_vscode/vscode_tutorial/src/format/main.ts
+++ vscode_tutorial_get_starting_with_vscode/vscode_tutorial/src/format/main.ts
@@ -1,2 +1,5 @@
-function greet(text: string): string { return `Hello, ${text}!` }
-const helloWorld = greet("world"); const helloPath = greet("path");
+function greet(text: string): string {
+  return `Hello, ${text}!`;
+}
+const helloWorld = greet("world");
+const helloPath = greet("path");
