0de5fd0a0a9bf3c806470ffa7d3c12ff9097a7f8
ShotaArima <g2172010@tcu.ac.jp>
2025-07-11 00:51:25 +0900

    add unit test

--- testing_test_count_word/testing/src/count_word/index.test.ts
+++ testing_test_count_word/testing/src/count_word/index.test.ts
@@ -1,2 +1,22 @@
 import {countWord} from "@/count_word";
 
+describe("determineGeneration", () => {
+  describe("on target test", () => {
+    test("text: Tokyo City, targetWord: Tokyo, count: 1", () => {
+      expect(countWord("Tokyo City", "Tokyo")).toBe(1);
+    });
+    test("text: Tokyo City in Tokyo, targetWord: Tokyo, count: 2", () => {
+      expect(countWord("Tokyo City in Tokyo", "Tokyo")).toBe(2);
+    });
+  });
+  describe("non target test", () => {
+    test("text: Tokyo City, targetWord: Yokohama, count: 0", () => {
+      expect(countWord("Tokyo City", "Yokohama")).toBe(0);
+    });
+  });
+  describe("empty string", () => {
+    test("text: , targetWord: Tokyo, count: -1", () => {
+      expect(countWord("", "Tokyo")).toBe(-1);
+    });
+  });
+});
