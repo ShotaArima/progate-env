fe7a8f583371ba406580ac4b501955191c471523
ShotaArima <g2172010@tcu.ac.jp>
2025-07-12 04:04:49 +0900

    create mock test

--- testing_test_get_user_name/testing/src/api/get_user_name.test.ts
+++ testing_test_get_user_name/testing/src/api/get_user_name.test.ts
@@ -1,3 +1,33 @@
 import {getUserName} from "@/api/get_user_name";
 import {fetchUser} from "@/api/fetch_user";
 
+jest.mock("@/api/fetch_user", () => {
+  return {
+    fetchUser: jest.fn().mockImplementation((id: number) => {
+      if (id === 1) {
+        return Promise.resolve({name: "James", email: "abc@def.com", id: 1});
+      } else {
+        return Promise.resolve(null);
+      }
+    }),
+  };
+});
+
+describe("getUserContent", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("returns correct data", async () => {
+    const ret = await getUserName(1);
+    expect(ret).toBe("James");
+    expect(fetchUser).toHaveBeenCalledWith(1);
+    expect(fetchUser).toBeCalledTimes(1);
+  });
+
+  test("returns null on fetchUser failure", async () => {
+    const ret = await getUserName(-1);
+    expect(ret).toBeNull();
+    expect(fetchUser).toHaveBeenCalledWith(-1);
+    expect(fetchUser).toBeCalledTimes(1);
+  });
+});
